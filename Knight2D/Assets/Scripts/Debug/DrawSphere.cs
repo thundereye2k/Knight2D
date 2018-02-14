d46d0f022bbd4df736d77fa632607fca19bbba33
Eric Downey <edowney29@gmail.com>
2018-02-14 14:34:59 -0500

    Major update

---
+++ Knight2D/Assets/Scripts/Debug/DrawSphere.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawSphere : MonoBehaviour {
+
+	public float radius;
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+}
